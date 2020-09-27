812fea1ebd72c087ebd5732bd11d2c9f503bf8f0
Abdullah Oğuz <oguzabdullah39@gmail.com>
2020-09-27 17:30:59 +0300

    news logic complate

--- routes/news.js
+++ routes/news.js
@@ -23,16 +23,66 @@ router.post("/", (req, res) => {
       res.json(data);
     })
     .catch((err) => {
-      res.json({ message: err });
+      res.json({ mesaage: err });
     });
 });
 
-router.delete("/:contentId", async (req, res) => {
+router.get("/:newsId", async (req, res) => {
   try {
-    const removedContent = await Content.remove({ _id: req.params.postId });
-    res.json(removedPost);
+    const news = await News.findById(req.params.newsId);
+    res.json(news);
   } catch (err) {
     res.json({ mesaage: err });
   }
 });
+
+router.delete("/:newsId", async (req, res) => {
+  try {
+    const removedNews = await News.remove({ _id: req.params.newsId });
+    res.json(removedNews);
+  } catch (err) {
+    res.json({ mesaage: err });
+  }
+});
+
+router.patch("/:newsId", async (req, res) => {
+  try {
+    const updateNews = await News.updateOne(
+      { _id: req.params.newsId },
+      { $set: { title: req.body.title } },
+      { $set: { image: req.body.image } },
+      { $set: { content: req.body.content } }
+    );
+    res.json(updateNews);
+  } catch (err) {
+    res.json({ mesaage: err });
+  }
+});
+
+function getById(_id) {
+    var deferred = Q.defer();
+    var dashboard = db.collection("dashboard");
+  
+    db.collection("dashboard")
+      .find({ user_id: ObjectId(_id) })
+      // *****
+      .toArray(function (err, user) {
+        console.log(user);
+        console.log(_id);
+  
+        if (err) deferred.reject(err);
+  
+        if (user) {
+          // return user (without hashed password)
+          deferred.resolve(_.omit(user, "hash"));
+        } else {
+          // user not found
+          deferred.resolve();
+        }
+      });
+  
+    return deferred.promise;
+  }
+
+
 module.exports = router;
