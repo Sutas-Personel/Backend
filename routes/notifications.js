a41392886bf0023e03253418d9e8a3cfad325393
Abdullah Oğuz <oguzabdullah39@gmail.com>
2020-09-27 13:31:17 +0300

    development branch initial commit

--- routes/notifications.js
+++ routes/notifications.js
@@ -3,11 +3,8 @@ const Notifications = require("../models/Notifications.js");
 
 const router = express.Router();
 
-
 router.get("/", (req, res) => {
-    const post = req.body;
-    res.json(post);
-    console.log(req.body);
-  });
-
-
+  const post = req.body;
+  res.json(post);
+  console.log(req.body);
+});
