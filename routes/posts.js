51b6f3d140785e89200b61a414253821a67d71d5
fatihemree <fatih.emre.212@gmail.com>
2020-09-27 03:00:03 +0300

    first commit

--- routes/posts.js
+++ routes/posts.js
@@ -1,62 +1,91 @@
-const express = require('express');
-const Post = require('../models/Post');
+const express = require("express");
+const Post = require("../models/Post");
 
 const router = express.Router();
 
-router.get('/' , (req,res) => {
-    res.send('burası posts mekanı')
+router.get("/", (req, res) => {
+  const post = req.body;
+  res.json(post);
+  console.log(req.body);
+
+  // res.send('burası posts mekanı')
 });
 
-router.post('/' , (req,res) => {
-    const post = new Post({
-        title: req.body.title,
-        description: req.body.description
-    });
+router.post("/", (req, res) => {
+  const post = new Post({
+    title: req.body.title,
+    description: req.body.description,
+  });
 
-    post.save()
-        .then(data => {
-            res.json(data);
-        })
-        .catch(err => {
-            res.json({mesaage : err})
-        });
+  post
+    .save()
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.json({ mesaage: err });
+    });
 });
 
 // Id göre arama
 
-router.get('/:postId', async (req,res) =>{
-    try{
-        const post = await Post.findById(req.params.postId);
-        res.json(post);
-    } catch (err) {
-        res.json({mesaage: err})
-    }
+router.get("/:postId", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.postId);
+    res.json(post);
+  } catch (err) {
+    res.json({ mesaage: err });
+  }
 });
 
 // delete post
 
-router.delete('/:postId', async (req,res) => {
-    try{
-        const removedPost = await Post.remove({ _id: req.params.postId });
-        res.json(removedPost);
-    } catch (err) {
-        res.json({mesaage: err})
-    }
+router.delete("/:postId", async (req, res) => {
+  try {
+    const removedPost = await Post.remove({ _id: req.params.postId });
+    res.json(removedPost);
+  } catch (err) {
+    res.json({ mesaage: err });
+  }
 });
 
 // update post
 
-router.patch('/:postId', async (req,res) => {
-    try{
-        const updatedPost = await Post.updateOne(
-            { _id: req.params.postId },
-            { $set : { title : req.body.title }}
-        );
-        res.json(updatedPost);
-    } catch (err) {
-        res.json({mesaage: err})
-    }
+router.patch("/:postId", async (req, res) => {
+  try {
+    const updatedPost = await Post.updateOne(
+      { _id: req.params.postId },
+      { $set: { title: req.body.title } }
+    );
+    res.json(updatedPost);
+  } catch (err) {
+    res.json({ mesaage: err });
+  }
 });
 
+function getById(_id) {
+  var deferred = Q.defer();
+  var dashboard = db.collection("dashboard");
+
+  db.collection("dashboard")
+    .find({ user_id: ObjectId(_id) })
+    // *****
+    .toArray(function (err, user) {
+      console.log(user);
+      console.log(_id);
+
+      if (err) deferred.reject(err);
+
+      if (user) {
+        // return user (without hashed password)
+        deferred.resolve(_.omit(user, "hash"));
+      } else {
+        // user not found
+        deferred.resolve();
+      }
+    });
+
+  return deferred.promise;
+}
 
 module.exports = router;
