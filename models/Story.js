ba61e907d01e63714b552becdf4b939fd7ae7cc8
Abdullah Oğuz <oguzabdullah39@gmail.com>
2020-09-28 09:29:54 +0300

    edit schema

---
+++ models/Story.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const storySchema = mongoose.Schema({
+  url: {
+    type: String,
+    required: true,
+  },
+  media: {
+    type: String,
+    enum: ["VIDEO", "IMAGE"],
+    default: "IMAGE",
+  },
+  duration: {
+    type: String,
+    required: true,
+  },
+
+  user: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Story", storySchema);
